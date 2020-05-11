4e1ee5bd2fef42265db6d690498ed001f049c93a
Enrique720 <enrique.sobrados@utec.edu.pe>
2020-05-11 02:08:23 -0500

    twitter with api

---
+++ twitter/run.py
@@ -0,0 +1,58 @@
+
+import tweepy
+from tweepy import OAuthHandler
+import json
+import wget
+#Twitter API credentials
+consumer_key = "eYyvdAokGZI089xU1MIwQtzON"
+consumer_secret = "9R9n5vB76Q1x57fpFuiLlgSFAKizN6NoY5NZDAW5O8acNPMGvN"
+access_token = "1259734035422724096-XuxXZiC6GAiIWiWItm5mGSDSr5FbTG"
+access_secret = "XKoXbah03fpBdJGMyimX3QRF8jPTnmn8GBnFSMt0SdtMz"
+
+@classmethod
+def parse(cls, api, raw):
+    status = cls.first_parse(api, raw)
+    setattr(status, 'json', json.dumps(raw))
+    return status
+ 
+# Status() is the data model for a tweet
+tweepy.models.Status.first_parse = tweepy.models.Status.parse
+tweepy.models.Status.parse = parse
+# User() is the data model for a user profil
+tweepy.models.User.first_parse = tweepy.models.User.parse
+tweepy.models.User.parse = parse
+# You need to do it for all the models you need
+
+ 
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+ 
+api = tweepy.API(auth)
+
+tweets = api.user_timeline(screen_name='miguelmalvarez',
+                           count=200, include_rts=False,
+                           exclude_replies=True)
+last_id = tweets[-1].id
+ 
+username = 'miguelmalvarez'
+while (True):
+    more_tweets = api.user_timeline(screen_name=username,
+                                count=200,
+                                include_rts=False,
+                                exclude_replies=True,
+                                max_id=last_id-1)
+    # There are no more tweets
+    if (len(more_tweets) == 0):
+        break
+    else:
+        last_id = more_tweets[-1].id-1
+        tweets = tweets + more_tweets
+
+media_files = set()
+for status in tweets:
+    media = status.entities.get('media', [])
+    if(len(media) > 0):
+        media_files.add(media[0]['media_url'])
+
+for media_file in media_files:
+    wget.download(media_file)
